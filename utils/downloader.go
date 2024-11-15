d2268495e17903b04f421c68b6b8c36dcb7a178d
aaochieng <ochiengaoron@outlook.com>
2024-11-15 11:04:19 +0300

    fix: moved flags parsing and Downloader functionality

---
+++ utils/downloader.go
@@ -0,0 +1,60 @@
+package utils
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func extractFileName(url string) string {
+	idx := strings.LastIndex(url, "/")
+	return url[idx+1:]
+}
+
+func (w *WgetValues) Downloader() {
+
+	// Perform the HTTP GET request
+	res, err := http.Get(w.Url)
+	CheckError(err)
+	defer res.Body.Close() // Ensure the body is closed after reading
+
+	/// If Output file is not given , extract the given filename from the metadata.
+	if w.OutputFile == "" {
+		w.OutputFile = extractFileName(w.Url)
+	}
+
+	// Check Background Mode if given
+	if !w.BackgroudMode {
+		fmt.Println("Download started at:")
+		timeStarted := time.Now().Format("2006-01-02 15:04:05")
+		fmt.Println("Started at: ", timeStarted)
+		fmt.Printf("sending request, awaiting response... status %s\n", res.Status)
+		fmt.Printf("content size: %d [~%.2fMB]\n", res.ContentLength, float64(float64(res.ContentLength)/1048576))
+		fmt.Printf("saving file to: ./%s\n", w.OutputFile)
+	}
+
+	// Create the output file
+	file, err := os.Create(w.OutputFile)
+	CheckError(err)
+	defer file.Close()
+
+	pr := &ProgressRecoder{
+		Reader:           res.Body,
+		Total:            res.ContentLength,
+		ProgressFunction: ShowProgress,
+	}
+
+	// Read the response body
+	_, err = io.Copy(file, pr)
+	CheckError(err)
+
+	// Completed downloading the file
+	if !w.BackgroudMode {
+		fmt.Println("Download completed at:", time.Now().Format("2006-01-02 15:04:05"))
+		os.Exit(0)
+	}
+	return
+}
--- utils/flag_parser.go
+++ utils/flag_parser.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"regexp"
 )
 
@@ -15,4 +16,43 @@ func (wg WgetValues) ValidateURL(url string) bool {
 
 	// Check if the URL matches the pattern
 	return re.MatchString(url)
+}
+
+func (w *WgetValues) FlagsParser(args []string) {
+	for i := 0; i < len(args); i++ {
+		arg := args[i]
+		switch arg {
+		case "-B":
+			w.BackgroudMode = true
+		case "-O":
+			if i+1 < len(args) {
+				w.OutputFile = args[i+1]
+				i++ // Skip the next element as it's the value for -O
+			}
+		case "-P":
+			if i+1 < len(args) {
+				w.OutPutDirectory = args[i+1]
+				i++
+			}
+		case "--rate-limit":
+			if i+1 < len(args) {
+				w.RateLimitValue = args[i+1]
+				i++
+			}
+		case "--reject":
+			w.Reject = true
+		case "--exclude", "-X":
+			if i+1 < len(args) {
+				w.Exclude = args[i+1]
+				i++
+			}
+		case "--convert-links":
+			w.ConvertLinks = true
+		case "--mirror":
+			w.Mirror = true
+		default:
+			fmt.Printf("Unknown argument: %s\n", arg)
+			return
+		}
+	}
 }
